48bb430fc212e1d47e77a44f724590e95ac3daf5
gabriel assunção <gabriel.costa@sga.pucminas.br>
2025-05-28 20:19:44 -0300

    Feat: adiciona método de registro de usuário

--- development/routes/api.php
+++ development/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
