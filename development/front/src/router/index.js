44959fcab39ba60daded38fc6c3948d1e81ff069
gabriel assunção <gabriel.costa@sga.pucminas.br>
2025-06-03 07:55:53 -0300

    Feat: adiciona login page

---
+++ development/front/src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginPage from '../pages/LoginPage.vue'
+import HomePage from '../pages/HomePage.vue'
+
+const routes = [
+    { path: '/', component: HomePage },
+    { path: '/login', component: LoginPage },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+export default router
