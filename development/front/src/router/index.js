3e39ce36d0cfa54ebd48033d759f99aaa574d33f
gabriel assunção <gabriel.costa@sga.pucminas.br>
2025-06-07 15:37:42 -0300

    feat: adiciona materiais de estudo ao sistema

--- development/front/src/router/index.js
+++ development/front/src/router/index.js
@@ -4,6 +4,7 @@ import HomePage from '../pages/HomePage.vue'
 import Register from '../pages/RegisterPage.vue'
 import QuizzListPage from '../pages/QuizzListPage.vue'
 import QuizzPage from '../pages/QuizzPage.vue'
+import PostListPage from '../pages/PostListPage.vue'
 
 const routes = [
     { path: '/', component: HomePage },
@@ -11,6 +12,7 @@ const routes = [
     { path: '/register', component: Register },
     { path: '/quizz-list', component: QuizzListPage },
     { path: '/quizzes/:id', component: QuizzPage, props: true },
+    { path: '/materiais-list', component: PostListPage },
 ]
 
 const router = createRouter({
